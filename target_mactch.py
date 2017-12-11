410be453a597173c5bfe7646e97e2ef3a28a3345
ChenyuSu624 <csu@csumb.edu>
2017-12-10 23:20:19 -0800

    Add comments

--- target_mactch.py
+++ target_mactch.py
@@ -1,12 +1,16 @@
 
 import csv, random, sys, string, locale
+
+# Asking user to input the target sequence, and then pass to the find_target function
 def main():
     temp = input("What is your target sequence? ")
     temp2 = find_target(temp)
     ##print(f"IDs that contain your target sequence are in the list : {temp2[0]}")
     return temp2
 
-
+# this function will taake the user's input as a parameter, and check whether the target sequence whether appear in each sequence.
+# If it does, the function will append the sequence Id into a list, and returns.
+# else, it will print Target sequence is not in data base."
 def find_target(target_seq):
     with open('seq_log.csv', 'r') as seq_log:
         keys = ['seq_ID', 'Sequence', 'Compliment']
@@ -21,7 +25,8 @@ def find_target(target_seq):
                 ID_list.append(target_seq)
                 return ID_list
         return "Target sequence is not in data base."
-
+    
+# This function just take the target sequence, calculate the numbers of AT and GC.
 def calculation(target_seq):
     AT_counter = 0;
     GC_counter = 0;
@@ -33,7 +38,5 @@ def calculation(target_seq):
             GC_counter += 1
     counter_list.append(AT_counter)
     counter_list.append(GC_counter)
-    
+
     return counter_list
-                        
-                
